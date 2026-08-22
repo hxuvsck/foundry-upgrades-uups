9ccbb3436fd3e2c7ba5bb80db4868db75ab0b986
hxuvsck <hxuvsck@gmail.com>
2026-08-22 22:10:06 +0800

    Upgrade deployment script with foundry-devops adding

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract UpgradeBox {}
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract UpgradeBox is Script {
+    function run() external returns (address) {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+    }
+}
