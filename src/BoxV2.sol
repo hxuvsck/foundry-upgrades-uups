28a2809b12d52c8b3490f3d78add7f7516346a32
hxuvsck <hxuvsck@gmail.com>
2026-08-24 12:17:08 +0800

    testUgrade encounter setNumber blank

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -22,7 +22,9 @@ contract BoxV2 is UUPSUpgradeable, Initializable, OwnableUpgradeable {
         __Ownable_init(msg.sender); // sets owner to: owner = msg.sender from access of upgrade for proxy scs.
     }
 
-    function setNumber(uint256 _number) external {}
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
